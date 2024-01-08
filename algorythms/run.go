0850367663d6fb85a36edb4da31177d5fff954e8
Selim <selimdev00@gmail.com>
2024-01-08 08:55:48 +0500

    [euclidean]: loop algorythm

--- algorythms/run.go
+++ algorythms/run.go
@@ -10,16 +10,27 @@ func main() {
 	fmt.Println("Type numbers: ")
 	fmt.Scan(&p, &q)
 
-	fmt.Printf("The result is: %v", euclidean(p, q))
+	fmt.Printf("The result is: %v", euclidean_loop(p, q))
 }
 
 // recursive algorythm, getting remainder and starting same functio with q and remainder
-func euclidean(p int, q int) int {
+func euclidean_recursive(p int, q int) int {
 	r := p % q
 
 	if r != 0 {
-		return euclidean(q, r)
+		return euclidean_recursive(q, r)
 	}
 	
 	return q
+}
+
+// looping algorythm, without recursive
+func euclidean_loop(p int, q int) int {
+	r := p % q
+
+	for r > 0 {
+		r = q % r
+	}
+	
+	return r
 }
